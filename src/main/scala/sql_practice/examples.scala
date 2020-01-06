7c00954b1c1d5a2a1119aca72847e9c41f20fb7d
Antoine LAURENDON <antoine.laurendon@ensg.eu>
2020-01-06 12:13:00 +0100

    SQL ex1

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -35,4 +35,20 @@ object examples {
 
 
   }
+  def exec2(): Unit = {
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val demo = spark.read.json("data/input/demographie_par_commune.json")
+    val dep = spark.read.csv("data/input/departements.txt").select($"_c0" as "name",$"_c1" as "id")
+
+    demo.select(sum($"population")).show
+
+    val depWithDemo = demo.select($"departement",$"population").join(dep,$"departement"===$"id").select($"name" as "departement",$"id" as "code", $"population").groupBy($"departement",$"code").agg(sum(($"population")) as "population")
+
+    depWithDemo.select($"code",$"population").orderBy($"population".desc).show
+
+    depWithDemo.select($"departement",$"population").orderBy($"population".desc).show
+
+  }
 }
